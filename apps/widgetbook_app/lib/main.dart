cad67969ddfd07d4cec4b4c73fb43b5c978f43f5
Youssef Andrawes <YoussefRaafatNasry@gmail.com>
2025-06-11 16:04:34 +0200

    fix structure

--- apps/widgetbook_app/lib/main.dart
+++ apps/widgetbook_app/lib/main.dart
@@ -1,30 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:ui/ui.dart';
+import 'package:ui/ui.dart' as ui;
 import 'package:widgetbook/widgetbook.dart';
-import 'package:widgetbook_annotation/widgetbook_annotation.dart';
-
-import 'main.directories.g.dart';
 
 void main() {
   // runApp(const MainApp());
   runApp(WidgetbookApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: Scaffold(body: Center(child: ExampleWidget())));
-  }
-}
-
-@App()
 class WidgetbookApp extends StatelessWidget {
+  const WidgetbookApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Widgetbook.material(
-      directories: directories,
-    );
+    return Widgetbook.material(directories: ui.directories);
   }
 }
